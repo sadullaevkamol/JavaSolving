3a0e47b1f232191a8126b7454564ea5405aae85f
Kamol <sadullaevkamol2004@gmail.com>
2021-11-21 09:21:55 +0500

    yyy

---
+++ script.js
@@ -0,0 +1,46 @@
+// name
+
+var name = prompt('Enter your name')
+
+alert(name)
+
+console.log(name);
+
+// age
+
+var age = +prompt('Enter your age')
+
+alert(age)
+
+console.log(age);
+
+
+// math solving
+
+var plus = +prompt('12 + 3 = ?')
+
+alert(plus)
+
+console.log('12 + 3 = 15 ' + ' Your answer is ' + plus);
+
+var minus = +prompt('12 - 3 = ?')
+
+alert(minus)
+
+console.log('12 - 3 = 9 ' + ' Your answer is ' + minus);
+
+var multiply = +prompt('12 * 3 = ?')
+
+alert(multiply)
+
+console.log('12 * 3 = 36' + ' Your answer is ' + multiply);
+
+var devide = +prompt('12 / 3 = ?')
+
+alert(devide)
+
+console.log('12 / 3 = 4' +  ' Your answer is ' + devide);
+
+// check the results
+
+alert('Enter the console to see your answers')
